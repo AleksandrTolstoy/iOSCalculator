1a9814e872b7dd9d0b1a0a83e6fc771195824034
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-05-11 15:52:51 +0300

    Initial Commit

---
+++ iOSCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSCalculator
+//
+//  Created by Александр Толстой on 11.05.2020.
+//  Copyright © 2020 Aleksandr Tolstoy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
