17fa5306c0dd48a5d099073396915f3585168df4
Aleksandr Tolstoy <aleksandr13tolstoy@gmail.com>
2020-05-11 16:25:11 +0300

    Added calculator grid

--- iOSCalculator/ContentView.swift
+++ iOSCalculator/ContentView.swift
@@ -9,8 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [
+        ["AC", "±", "%", "÷"],
+        ["7", "8", "9", "×"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", "F", "="]
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ForEach(buttons, id: \.self) { row in
+                HStack {
+                    ForEach(row, id: \.self) {button in
+                        Text(button)
+                        .font(.system(size: 32))
+                        .frame(width: 80, height: 80)
+                    }
+                }
+            }
+        }
     }
 }
 
